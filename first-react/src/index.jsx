6a16202e573c222f3de1de5e08c0070e2b73cff8
hugomes14 <hugo.gomes1415@gmail.com>
2025-01-10 10:05:43 +0000

    lesson 12

--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -1,12 +1,24 @@
-import { createElement } from "react"
 import { createRoot } from "react-dom/client"
 
-
-const root = createRoot(document.getElementById("root"))
-
-
+const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <img className="img" src='src\assets\react.svg' />
+    <main>
+        <img src="src/assets/react.svg" width='40px' alt="React logo" />
+        <h1>Fun facts about React</h1>
+        <ul>    
+            <li>Was first released in 2013</li>
+            <li>Was dfd</li>
+            <li>Was dfd</li>
+            <li>Was dfd</li>
+            <li>Was dfd</li>
+        </ul>
+    </main>
 )
 
+/* "puts them in the html doc"
+it should return a object related with the creatElement function of react
+it need to be wrapped around in some other structure
+declarative means that one line of code runs multiple tasks, impearative means you need to write all the tusks by yourself
+composable means that a project or some thing else is made of parts that can be reusable in other situation, instead of be made from a single unreusable part
+*/
