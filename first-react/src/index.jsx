753189afc7a8f04d1e62a389c129dd415527e1d2
hugomes14 <hugo.gomes1415@gmail.com>
2025-01-10 09:23:51 +0000

    set up react project

---
+++ first-react/src/index.jsx
@@ -0,0 +1,12 @@
+import { createElement } from "react"
+import { createRoot } from "react-dom/client"
+
+
+const root = createRoot(document.getElementById("root"))
+
+
+
+root.render(
+    <img className="img" src='src\assets\react.svg' />
+)
+
