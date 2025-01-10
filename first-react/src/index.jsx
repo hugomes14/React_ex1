f5256ce57e6ad0d09b38b30ffc92c6e2d29f8ee0
hugomes14 <hugo.gomes1415@gmail.com>
2025-01-10 11:14:24 +0000

    lesson 14

--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -5,6 +5,10 @@ const root = createRoot(document.querySelector('#root'))
 
 function Page() {
     return (
+    <div>
+        <header>
+            <img src="src/assets/react.svg" width='40px' alt="React Logo" />
+        </header>
         <main>
             <h1>Reasons to be excited to be learning react</h1>
             <ol>
@@ -17,6 +21,8 @@ function Page() {
                 <li>some thing</li>
             </ol>
         </main>
+        <footer>c 20xx Gomes development. All rights reserved</footer>
+    </div>
     )
 }
 
